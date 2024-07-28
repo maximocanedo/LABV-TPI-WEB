07414b9b35f66f44ad4e434e7cedc1d1e1cbf725
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2024-07-27 21:33:57 -0300

    Librería de diseño

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {User} from "./entity/users";
+import * as users from "./actions/users";
+import './globals.css';
+import './output.css';
+import { Button } from "./components/ui/button"
+
 
 const App: React.FC = () => {
+    const [me, setCurrentUser] = useState<User | null>(null);
+    const loadCurrentUser = async () => {
+        users.myself()
+            .then(setCurrentUser)
+            .catch(console.error)
+    };
+    const loginTest = async () => {
+        users.login("root", "12345678")
+            .then(_response => loadCurrentUser())
+            .catch(console.error);
+    }
     return (
         <div className="App">
-            <h1>Hello, world!</h1>
+            dsfsdf
+            <Button >Login</Button>
         </div>
     );
 };
--- src/events.ts
+++ src/events.ts
@@ -1,6 +1,9 @@
 'use strict';
 /// <reference path="./types/entities" />
 
+import {HttpMethod} from "./auth";
+import {CommonException} from "./entity/commons";
+
 /**
  * El evento onConnectionFailure se lanza cuando una solicitud HTTP no 
  * puede realizarse correctamente por problemas de conexión.
@@ -9,7 +12,7 @@
  * @param {JSON} body Cuerpo de la solicitud.
  * @param {TypeError} error Excepción obtenida.
  */
-export const emitConnectionFailure = (url, method, body, error) => {
+export const emitConnectionFailure = (url: string, method: HttpMethod, body: any, error: TypeError) => {
     const event = new CustomEvent('onConnectionFailure', {
         detail: { url, method, body, error }
     });
@@ -20,8 +23,8 @@ export const emitConnectionFailure = (url, method, body, error) => {
  * El evento onAPIException se lanza cuando el servidor responde con un error controlado.
  * @param {CommonException} error Error devuelto por el servidor.
  */
-export const emitAPIException = (error) => {
-    const event = new CustomEvent('onAPIException', {
+export const emitAPIException = (error: CommonException): void => {
+    const event: CustomEvent<CommonException> = new CustomEvent('onAPIException', {
         detail: error
     });
     document.dispatchEvent(event);
