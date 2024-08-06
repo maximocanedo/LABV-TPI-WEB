738211b2aff068fae575fe313004d1c0ff5ff8bf
Lucas Gruden <73834229+Lucas-G-Lunix@users.noreply.github.com>
2024-08-06 09:34:29 -0300

    Se agrego react hook form

--- src/components/page/reports/MainReportPage.tsx
+++ src/components/page/reports/MainReportPage.tsx
@@ -1,64 +1,67 @@
 'use strict';
-import { useCurrentUser } from "src/components/users/CurrentUserContext";
-import { PageContent } from "../commons/PageContent";
 import { Bar, BarChart, CartesianGrid, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import { PageContent } from "../commons/PageContent";
 import { Header } from "../commons/Header";
+import { useCurrentUser } from "src/components/users/CurrentUserContext";
 import { Button } from "src/components/ui/button";
 import { SearchIcon } from "lucide-react";
-import * as Label from "@radix-ui/react-label";
-import { Form } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 const data = [
     {
         name: 'Domingo',
-        pv: 36,
+        pv: 6,
     },
     {
         name: 'Lunes',
-        pv: 19,
+        pv: 10,
     },
     {
         name: 'Martes',
-        pv: 29,
+        pv: 11,
     },
     {
         name: 'Miercoles',
-        pv: 59,
+        pv: 7,
     },
     {
         name: 'Jueves',
-        pv: 56,
+        pv: 10,
     },
     {
         name: 'Viernes',
-        pv: 80,
+        pv: 9,
     },
     {
         name: 'Sabado',
-        pv: 48,
+        pv: 10,
     },
 ]
 
+type Inputs = {
+    startDate: Date
+    endDate: Date
+    appoinmentStatus: string
+}
+
 export const MainReportPage = () => {
     const { me, can } = useCurrentUser();
+    const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
     return (
         <>
             <Header>Reportes</Header>
             <PageContent>
-                <form>
-                    <Label.Root className="LabelRoot" htmlFor="startDate">
-                        Start Date
-                    </Label.Root>
-                    <input className="Input" type="date" id="startDate" />
-                    <Label.Root className="LabelRoot" htmlFor="endDate">
-                        End Date
-                    </Label.Root>
-                    <input className="Input" type="date" id="endDate" />
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <label htmlFor="startDate">Start Date</label>
+                    <input className="Input" type="date" id="startDate" {...register("startDate", { required: true })} />
+                    <label htmlFor="endDate">End Date</label>
+                    <input className="Input" type="date" id="endDate" {...register("endDate", { required: true })}/>
                     {/* <AppointmentStatusFilterControl></AppointmentStatusFilterControl> */}
                     <Button><SearchIcon />Buscar</Button>
                 </form>
                 <div className="flex justify-between gap-2 w-full">
-                    <ResponsiveContainer width="50%" height={400}>
+                    <ResponsiveContainer width="75%" height={400}>
                         <BarChart
                             width={500}
                             height={300}
