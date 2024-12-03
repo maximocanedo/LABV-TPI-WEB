9d17fd987c171f774378283fcd0f4bce411c6ec0
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2024-12-02 23:55:19 -0300

    Logos y links

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import {Toaster} from "./components/ui/toaster";
 import Link from "./components/text/Link";
 import {useToast} from "./components/ui/use-toast";
 import {
-    Bell,
+    Bell, Github, Hospital,
     Package2
 } from "lucide-react";
 import {LateralMenu} from "./components/page/commons/LateralMenu";
@@ -32,6 +32,13 @@ import {TestPage} from "./components/TestPage";
 import {AppointPage} from "./components/page/appointments/AppointPage";
 import {AppointmentPage} from "./components/page/appointments/AppointmentPage";
 import {Appointments} from "./components/page/appointments/Appointments";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuTrigger
+} from './components/ui/dropdown-menu';
 
 export type CurrentUser = User | null | "loading";
 
@@ -46,13 +53,31 @@ const App: React.FC = () => {
                     <div className="flex h-full max-h-screen flex-col gap-2">
                         <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
                             <Link href="/" className="flex items-center gap-2 font-semibold">
-                                <Package2 className="h-6 w-6"/>
+                                <Hospital className="h-6 w-6"/>
                                 <span className="">LABV-TPI</span>
                             </Link>
-                            <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
-                                <Bell className="h-4 w-4"/>
-                                <span className="sr-only">Toggle notifications</span>
-                            </Button>
+
+                            <DropdownMenu>
+                                <DropdownMenuTrigger asChild>
+                                    <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
+                                        <Github className="h-4 w-4"/>
+                                        <span className="sr-only">Repositorios</span>
+                                    </Button>
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent>
+                                    <DropdownMenuLabel>Repositorios</DropdownMenuLabel>
+                                    <DropdownMenuItem onClick={() => {
+                                        window.open("https://github.com/maximocanedo/LABV-TPI-WEB", "_blank")
+                                    }}>
+                                        Frontend
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem onClick={() => {
+                                        window.open("https://github.com/maximocanedo/LABV-TP4", "_blank")
+                                    }}>
+                                        API
+                                    </DropdownMenuItem>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
                         </div>
                         <div className="flex-1">
                             <LateralMenu isInModal={false} />
